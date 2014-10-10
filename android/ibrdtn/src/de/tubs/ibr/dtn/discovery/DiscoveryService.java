7de35e186ccffc7961115376a2f5d2ab5184ad4d
Henning Post <henningpost@gmail.com>
2014-10-10 15:15:53 +0200

    replaced BLE scan with WiFi scan for evaluation

--- android/ibrdtn/src/de/tubs/ibr/dtn/discovery/DiscoveryService.java
+++ android/ibrdtn/src/de/tubs/ibr/dtn/discovery/DiscoveryService.java
@@ -1,5 +1,6 @@
 package de.tubs.ibr.dtn.discovery;
 
+import java.util.List;
 import java.util.Locale;
 
 import android.annotation.SuppressLint;
@@ -7,11 +8,11 @@ import android.annotation.TargetApi;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.app.Service;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
@@ -23,7 +24,6 @@ import android.preference.PreferenceManager;
 import android.support.v4.content.WakefulBroadcastReceiver;
 import android.util.Log;
 import android.util.SparseArray;
-
 import de.tubs.ibr.dtn.daemon.Preferences;
 
 @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR2)
@@ -36,43 +36,52 @@ public class DiscoveryService extends Service {
      */
     public static final String DTN_DISCOVERY_PREFIX = "dtn:";
     
+    /**
+     * Time in ms before a previously found node is announced again
+     */
+    public static final int REDISCOVER_TIMEOUT = 5 * 60 * 1000;
+    
     public static final String ACTION_BLE_NODE_DISCOVERED = "de.tubs.ibr.dtn.Intent.BLE_NODE_DISCOVERED";
     public static final String EXTRA_BLE_NODE = "de.tubs.ibr.dtn.Intent.BLE_NODE";
 
     private Handler mHandler = new Handler();
-    private BluetoothManager mBluetoothManager;
-    private BluetoothAdapter mBluetoothAdapter;
     private WifiManager mWifiManager;
     private AlarmManager mAlarmManager;
     private Intent mWakefulIntent;
     private SparseArray<Long> mDiscoveredDtnNodes;
-
-    private BluetoothAdapter.LeScanCallback mLeScanCallback =
-            new BluetoothAdapter.LeScanCallback() {
-                @Override
-                public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
-                    Log.i(TAG, "onLeScan: " + device.getAddress() + " (rssi=" + rssi + ")");
-                    if (device.getName().toLowerCase(Locale.US).startsWith(DTN_DISCOVERY_PREFIX)) {
-                        onDtnNodeDiscovered(device.getAddress());
-                    }
-                }
-            };
+    
+    private BroadcastReceiver mWifiReceiver = new BroadcastReceiver() {
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			// TODO unregister receiver?
+			unregisterReceiver(mWifiReceiver);
+			
+			List<ScanResult> results = mWifiManager.getScanResults();
+			for (ScanResult s : results) {
+				Log.i(TAG, "onWifiScan: " + s.SSID + " (" + s.level + ")");
+				if (s.SSID.toLowerCase(Locale.US).startsWith(DTN_DISCOVERY_PREFIX)) {
+					onDtnNodeDiscovered(s.SSID);
+				}
+				
+			}
+		}
+    };
 
     private void onDtnNodeDiscovered(String address) {
 
     	// transform address to proper format
-    	String nodeAddress = address.toLowerCase(Locale.US).replace(":", "");
+    	String nodeAddress = address.toLowerCase(Locale.US);
 
-        // throttle announcements to once per minute
+        // throttle announcements
         int nodeHash = nodeAddress.hashCode();
         Long lastDiscoveryTime = mDiscoveredDtnNodes.get(nodeHash);
-        if (lastDiscoveryTime != null && System.currentTimeMillis() < lastDiscoveryTime + 60*1000 ) {
+        if (lastDiscoveryTime != null && System.currentTimeMillis() < lastDiscoveryTime + REDISCOVER_TIMEOUT) {
             return;
         }
         mDiscoveredDtnNodes.put(nodeHash, System.currentTimeMillis());
 
         Log.d(TAG, "found DTN node with address: " + nodeAddress);
-        String ssid = DTN_DISCOVERY_PREFIX + "//" + nodeAddress;
+        String ssid = nodeAddress;
         
 		// connect to network with matching SSID if not already connected
         WifiInfo wifiInfo = mWifiManager.getConnectionInfo();
@@ -120,57 +129,32 @@ public class DiscoveryService extends Service {
     @TargetApi(18)
     public boolean initialize() {
 
-        if (Build.VERSION.SDK_INT < 18) {
-          // Bluetooth Low Energy is not supported
-          return false;
-        }
-
-        // For API level 18 and above, get a reference to BluetoothAdapter through
-        // BluetoothManager.
-        if (mBluetoothManager == null) {
-            mBluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
-            if (mBluetoothManager == null) {
-                Log.e(TAG, "Unable to initialize BluetoothManager.");
-                return false;
-            }
-        }
-
-        mBluetoothAdapter = mBluetoothManager.getAdapter();
-        if (mBluetoothAdapter == null) {
-            Log.e(TAG, "Unable to obtain a BluetoothAdapter.");
-            return false;
-        }
-
         mAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
         mWifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
         mDiscoveredDtnNodes = new SparseArray<Long>();
-
+        
         return true;
     }
 
     @SuppressLint("NewApi")
 	private void startScan() {
       // TODO set proper values for scan duration and delay
-        final int duration = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString(Preferences.KEY_SCAN_DURATION, "2200"));
-        final int delay = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString(Preferences.KEY_SCAN_DELAY, "7800"));
+        final int delay = Integer.parseInt(PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getString(Preferences.KEY_SCAN_DELAY, "30000"));
 
-        if (duration <= 0) {
-            Log.i(TAG, "scan duration invalid: " + duration + " ms");
-            return;
-        }
-        Log.i(TAG, "scanning for " + duration + " ms");
-        mBluetoothAdapter.startLeScan(mLeScanCallback);
+        Log.i(TAG, "scanning for WiFi");
+        
+        registerReceiver(mWifiReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        mWifiManager.startScan();
+        
         mHandler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                mBluetoothAdapter.stopLeScan(mLeScanCallback);
-                
+         
                 if (!PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).getBoolean(Preferences.KEY_BLE_ENABLED, false))
                     return;
                 
                 // schedule new scan with AlarmManager
-                Log.i(TAG, "scanning again in " + delay + " ms");
                 PendingIntent intent = PendingIntent.getBroadcast(getApplicationContext(), 0, new Intent(WakefulDiscoveryReceiver.ACTION_BLE_DISCOVERY), 0);
                 if (Build.VERSION.SDK_INT >= 19) {
                     mAlarmManager.setExact(AlarmManager.ELAPSED_REALTIME_WAKEUP, SystemClock.elapsedRealtime() + delay, intent);
@@ -185,7 +169,7 @@ public class DiscoveryService extends Service {
                 	Log.e(TAG, "WakefulIntent is null!");
                 }
             }
-        }, duration);
+        }, delay);
     }
 
 }
